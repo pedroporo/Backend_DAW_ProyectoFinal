ada9fd03e9c65116404a1ee1e1b7071a603c953a
pedroporo <pedroguillferri9@gmail.com>
2025-02-03 19:31:19 +0100

    Patient Model and migratin done

---
+++ app/View/Components/Patient.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+enum Caso{
+    case Hola;
+}
+class Patient extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public string $name,
+        public string $last_name,
+        public string $birth_date,
+        public string $address,
+        public string $city,
+        public int $postal_code,
+        public string $dni,
+        public int $health_card_number,
+        public string $phone,
+        public string $email,
+        public int $zone_id,
+        public string $personal_situation,
+        public string $health_situation,
+        public string $housing_situation,
+        public string $personal_autonomy,
+        public string $economic_situation
+    ) {
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.patient');
+    }
+}
---
+++ database/migrations/2025_02_03_175222_crear_tabla_patient.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patient', function (Blueprint $table) {
+            $table->id()->unique();
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('birth_date');
+            $table->string('address');
+            $table->string('city');
+            $table->integer('postal_code');
+            $table->string('dni');
+            $table->integer('health_card_number')->unique();
+            $table->string('phone');
+            $table->string('email');
+            $table->integer('zone_id');
+            $table->string('personal_situation');
+            $table->string('health_situation');
+            $table->string('housing_situation');
+            $table->string('personal_autonomy');
+            $table->string('economic_situation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patient');
+    }
+};
