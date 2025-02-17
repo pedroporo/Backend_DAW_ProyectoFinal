25e009e319d684a208bd29774fa959e7c738ded8
pedroporo <pedroguillferri9@gmail.com>
2025-02-17 19:58:54 +0100

    test

---
+++ deploy.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Deployer;
+
+require 'vendor/deployer/deployer/recipe/laravel.php';
+
+set('application', 'Backend API');
+set('repository', 'git@github.com:pedroporo/Backend_DAW_ProyectoFinal.git');
+set('git_tty', true);
+set('keep_releases', 5);
+add('shared_dirs', ['./storage', './bootstrap/cache']);
+add('shared_files', ['.env']);
+add('writable_dirs', ['./storage', './bootstrap/cache']);
+host('backend.worldmemistic.duckdns.org')->set('remote_user', 'deployer')
+    ->set('port','22002')
+    ->set('identity_file', '~/.ssh/id_rsa')
+    ->set('deploy_path', '/var/www/api');
+
+task('build', function () {
+    run('cd {{release_path}} && npm install && npm run build');
+});
+task('reload:php-fpm', function () {
+    run('sudo /etc/init.d/php8.4-fpm restart');
+});
+
+after('deploy:failed', 'deploy:unlock');
+after('deploy', 'reload:php-fpm');
+before('deploy:symlink', 'artisan:migrate:fresh');
+before('deploy:symlink', 'artisan:db:seed');
+before('deploy:symlink', 'artisan:storage:link');
