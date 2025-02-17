da2ac0179940c87ca6b8954a4cf061d26082914a
pedroporo <pedroguillferri9@gmail.com>
2025-02-17 20:08:59 +0100

    test

--- deploy.php
+++ deploy.php
@@ -5,15 +5,13 @@ namespace Deployer;
 require 'vendor/deployer/deployer/recipe/laravel.php';
 
 set('application', 'Backend API');
-set('repository', 'git@github.com:pedroporo/Backend_DAW_ProyectoFinal.git');
-set('git_tty', true);
 set('keep_releases', 5);
 add('shared_dirs', ['./storage', './bootstrap/cache']);
 add('shared_files', ['.env']);
 add('writable_dirs', ['./storage', './bootstrap/cache']);
 host('backend.worldmemistic.duckdns.org')->set('remote_user', 'deployer')
     ->set('port','22002')
-
+    ->set('identity_file', '~/.ssh/id_rsa')
     ->set('deploy_path', '/var/www/api');
 
 task('build', function () {
