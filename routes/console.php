2bdb6ccba6c1fadaca848044a0ac81d15b59d680
pedroporo <pedroguillferri9@gmail.com>
2025-02-18 19:07:28 +0100

    schedule test

--- routes/console.php
+++ routes/console.php
@@ -6,4 +6,6 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-Schedule::command('sanctum:prune-expired --hours=24')->daily();
+Schedule::call(function () {
+    Schedule::command('sanctum:prune-expired --hours=24')->daily();
+})->daily();
