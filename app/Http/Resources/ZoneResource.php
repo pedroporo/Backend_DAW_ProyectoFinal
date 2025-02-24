536555ec277164245f1b8aec070a2645014241cc
pedroporo <pedroguillferri9@gmail.com>
2025-02-24 10:07:15 +0100

    fdgdf

--- app/Http/Resources/ZoneResource.php
+++ app/Http/Resources/ZoneResource.php
@@ -10,7 +10,7 @@ class ZoneResource extends JsonResource
     /**
      * @OA\Schema(
      *     schema="ZoneResource",
-     *     description="Esquema del recurs Zone",
+     *     description="Esquema del recurso Zone",
      *      @OA\Property(property="name", type="string", maxLength=255, description="Nombre de la zona", example="439 Karley Loaf Suite 897")
      * )
      */
