79ead9b5ad71d6cfb76a6ef201a106a56bb2a30b
pedroporo1000 <pedroguillferri9@gmail.com>
2025-02-24 15:40:23 +0100

    remove unique

--- app/Http/Requests/PatientRequest.php
+++ app/Http/Requests/PatientRequest.php
@@ -79,7 +79,7 @@ class PatientRequest extends FormRequest
             'city' => 'required',
             'postal_code' => 'required',
             'dni' => 'required',
-            'health_card_number' => 'required|unique:patients,health_card_number,'. $this->route('patient')->health_card_number,
+            'health_card_number' => 'required',
             'phone' => 'required',
             'email' => 'required',
             'zone_id' => 'required',
