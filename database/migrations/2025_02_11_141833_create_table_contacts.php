4f548653717c1bf0a531be5a06a03e44c453b090
pedroporo1000 <pedroguillferri9@gmail.com>
2025-02-24 16:15:51 +0100

    test

--- database/migrations/2025_02_11_141833_create_table_contacts.php
+++ database/migrations/2025_02_11_141833_create_table_contacts.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('phone');
-            $table->foreignId('patient_id')->constrained();
+            $table->foreignId('patient_id')->constrained()->onDelete('cascade');
             $table->string('relationship');
             $table->timestamps();
         });
